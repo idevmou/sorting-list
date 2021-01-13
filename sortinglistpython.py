991ae8bf827a7569879be42acdd40242647a981f
Mouli Sankar <67406552+MouliMou@users.noreply.github.com>
2021-01-13 14:13:52 +0530

    Add files via upload

---
+++ sortinglistpython.py
@@ -0,0 +1,6 @@
+alphabets = ["X","Q","C","S","E","O","G","H","I","J","Z","L","M","N","F","P","B","R","D","T","U","V","W","A","Y","K"] 
+
+# Sorting list of sstrings in ascending
+alphabets.sort()
+
+print(alphabets)
